052c1c4beb8053acb61fed92371a4110f2e8cc45
marasolo <marasolo@student.42antananarivo.mg>
2026-06-29 22:47:19 +0300

    06

---
+++ alchemy/grimoire/light_spellbook.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# ########################################################################### #
+#   shebang: 1                                                                #
+#                                                          :::      ::::::::  #
+#   light_spellbook.py                                   :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: marasolo <marasolo@student.42antananarivo.   +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/06/29 22:44:41 by marasolo            #+#    #+#            #
+#   Updated: 2026/06/29 22:46:11 by marasolo           ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+def light_spell_allowed_ingredients():
+def light_spell_record(spell_name: str, ingredients: str):
---
+++ alchemy/grimoire/light_validator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# ########################################################################### #
+#   shebang: 1                                                                #
+#                                                          :::      ::::::::  #
+#   light_validator.py                                   :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: marasolo <marasolo@student.42antananarivo.   +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/06/29 22:46:23 by marasolo            #+#    #+#            #
+#   Updated: 2026/06/29 22:46:43 by marasolo           ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+def validate_ingredients(ingredients: str):
